319fe67db85964fe0a5305b872ba0b80508d727e
ochieng <cindycynthia387@gmail.com>
2023-03-22 15:59:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const elementToRemove=document.querySelector('#main');
+elementToRemove.remove();
+
+const newHeader=document.createElement('h1');
+newHeader.id= 'victory';
+newHeader.textContent= 'YOUR-NAME is the champion';
+document.body.appendChild(newHeader);
+const yourName='Cynthia';
+newHeader.textContent= yourName + 'is the champion';
